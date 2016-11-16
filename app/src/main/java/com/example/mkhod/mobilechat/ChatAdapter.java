00bbfce28c2b8883c2cb6fbe2e866c2bcc0cdf52
mkhod <mkhodachnyk@gmail.com>
2016-11-16 18:07:16 +0200

    Message sending functionality

---
+++ app/src/main/java/com/example/mkhod/mobilechat/ChatAdapter.java
@@ -0,0 +1,75 @@
+package com.example.mkhod.mobilechat;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.List;
+
+/**
+ * Created by mkhod on 16.11.2016.
+ */
+
+public class ChatAdapter extends BaseAdapter {
+
+    private static LayoutInflater inflater = null;
+    List<Message> messages;
+
+    public ChatAdapter(Context context, List<Message> messages) {
+        this.messages = messages;
+        inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+    }
+
+    @Override
+    public int getCount() {
+        return messages.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return messages.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        Message message = messages.get(position);
+        View view = convertView;
+        if (convertView == null)
+            view = inflater.inflate(R.layout.chat_bubble, null);
+
+        TextView messageTextView = (TextView) view.findViewById(R.id.message_text);
+        messageTextView.setText(message.getText());
+
+        LinearLayout layout = (LinearLayout) view.findViewById(R.id.bubble_layout);
+
+        LinearLayout parent_layout = (LinearLayout) view.findViewById(R.id.bubble_layout_parent);
+
+        if (message.isMyMessage()) {
+            layout.setBackgroundResource(R.drawable.bubble2);
+            parent_layout.setGravity(Gravity.RIGHT);
+        } else {
+            layout.setBackgroundResource(R.drawable.bubble1);
+            parent_layout.setGravity(Gravity.LEFT);
+        }
+        messageTextView.setTextColor(Color.BLACK);
+
+        return view;
+
+    }
+
+    public void add(Message message) {
+        messages.add(message);
+    }
+
+}
--- app/src/main/java/com/example/mkhod/mobilechat/UserChatFragment.java
+++ app/src/main/java/com/example/mkhod/mobilechat/UserChatFragment.java
@@ -1,6 +1,14 @@
 package com.example.mkhod.mobilechat;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.view.LayoutInflater;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
 
 import java.util.UUID;
 
@@ -8,7 +16,66 @@ import java.util.UUID;
  * Created by mkhod on 16.11.2016.
  */
 public class UserChatFragment extends android.support.v4.app.Fragment {
+
+    public static ChatAdapter chatAdapter;
+
     private static final String ARG_USER_ID = "user_id";
+    private ChatUser user;
+
+    private EditText messageInputEditText;
+    private TextView userNameTitleTextView;
+    private ListView chatListView;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        UUID userId = (UUID) getArguments().getSerializable(ARG_USER_ID);
+        user = UserLab.getInstance(getActivity()).getUser(userId);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_user_chat, container, false);
+
+        messageInputEditText = (EditText) view.findViewById(R.id.message_edit_text);
+        messageInputEditText.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                final int DRAWABLE_RIGHT = 2;
+
+                if (event.getAction() == MotionEvent.ACTION_UP) {
+                    if (event.getRawX() >= (messageInputEditText.getRight() - messageInputEditText
+                            .getCompoundDrawables()[DRAWABLE_RIGHT].getBounds().width())) {
+                        sendMessage(messageInputEditText.getText().toString());
+                        return true;
+                    }
+                }
+                return false;
+            }
+
+        });
+
+
+        userNameTitleTextView = (TextView) view.findViewById(R.id.chat_user_name_title);
+        userNameTitleTextView.setText(user.getName());
+
+        chatListView = (ListView) view.findViewById(R.id.message_list_view);
+        chatListView.setTranscriptMode(ListView.TRANSCRIPT_MODE_ALWAYS_SCROLL);
+        chatListView.setStackFromBottom(true);
+
+        chatAdapter = new ChatAdapter(getActivity(), user.getMessages());
+        chatListView.setAdapter(chatAdapter);
+
+        return view;
+    }
+
+    private void sendMessage(String text) {
+        Message message = new Message(text, true);
+        user.addMessage(message);
+        messageInputEditText.setText("");
+        chatAdapter.notifyDataSetChanged();
+    }
 
     public static UserChatFragment newInstance(UUID userId) {
         Bundle args = new Bundle();
